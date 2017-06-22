96e13993f48d8c54688cc7c35772c7b5d6f19bbc
Travis CI <travis-ci@atomist.com>
2017-06-22 21:12:11 +0000

    Update spring-rest-seed from start.spring.io

---
+++ src/main/java/com/atomist/springteam/springrestseed/SpringRestSeedApplication.java
@@ -0,0 +1,12 @@
+package com.atomist.springteam.springrestseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSeedApplication.class, args);
+	}
+}
